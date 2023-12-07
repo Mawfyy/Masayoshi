33e9e98ea5468d8906936d8d4381417ad24cb8c6
Mawfyy <94380448+Mawfyy@users.noreply.github.com>
2023-12-07 17:51:03 -0500

    feat: now_playing done

--- src/music/now_playing.rs
+++ src/music/now_playing.rs
@@ -18,23 +18,59 @@ pub async fn now_playing(ctx: Context<'_>) -> CommandResult {
         return Ok(());
     };
 
-    let current_minute_track = formatter_minutes_second_format(
-        Duration::from_millis(current_track.info.position).as_secs(),
-    );
-
     let max_track_duration =
         formatter_minutes_second_format(Duration::from_millis(current_track.info.length).as_secs());
+    let track_title = current_track
+        .info
+        .title
+        .replace(&current_track.info.author, "");
+
+    if let Some(uri) = current_track.info.uri.clone() {
+        if uri.contains("youtube") {
+            println!("{}", uri);
+            ctx.send(|message| {
+                message.ephemeral(true).reply(true).embed(|embed| {
+                    embed
+                        .color(Color::BLUE)
+                        .description(format!(
+                            "{}\n**Duration**: `{}`\n**Source**: [{}]({})",
+                            current_track.info.title,
+                            max_track_duration,
+                            current_track.info.source_name,
+                            uri
+                        ))
+                        .thumbnail(current_track.info.artwork_url.unwrap())
+                        .footer(|footer| {
+                            footer
+                                .text("Powered by Mistik")
+                                .icon_url(ctx.cache().current_user().avatar_url().unwrap())
+                        })
+                })
+            })
+            .await?;
+
+            return Ok(());
+        }
+    }
 
     ctx.send(|message| {
         message.ephemeral(true).reply(true).embed(|embed| {
             embed
                 .color(Color::BLUE)
                 .description(format!(
-                    "{} | [{}/{}]",
-                    current_track.info.author, current_minute_track, max_track_duration
+                    "{}\n**Duration**:`{}`\n**Source**: [{}]({})",
+                    track_title,
+                    max_track_duration,
+                    current_track.info.source_name,
+                    current_track.info.uri.unwrap()
                 ))
                 .thumbnail(current_track.info.artwork_url.unwrap())
-                .author(|author| author.name(current_track.info.author))
+                .author(|author| author.name(format!("**{}**", current_track.info.author)))
+                .footer(|footer| {
+                    footer
+                        .text("Powered by Mistik")
+                        .icon_url(ctx.cache().current_user().avatar_url().unwrap())
+                })
         })
     })
     .await?;
@@ -43,18 +79,23 @@ pub async fn now_playing(ctx: Context<'_>) -> CommandResult {
 }
 
 fn formatter_minutes_second_format(track_duration: u64) -> String {
-    let minutes = track_duration / 60;
-    let seconds = track_duration % 60;
+    let minutes = (track_duration / 60).to_string();
+    let mut seconds = (track_duration % 60).to_string();
+
+    if seconds == "0" {
+        seconds = "00".to_string();
+    }
+
     format!("{}:{:.2}", minutes, seconds)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::music::now_playing::formatter_minutes_second_format;
+    use super::formatter_minutes_second_format;
 
     #[test]
-    fn it_should_return_minutes_format() {
-        let minutes = formatter_minutes_second_format(220);
-        assert_eq!("3:40", minutes);
+    fn it_should_return_max_duration_song() {
+        let result = formatter_minutes_second_format(180);
+        assert_eq!("3:00", result);
     }
 }
