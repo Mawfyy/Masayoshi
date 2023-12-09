a613aa2d2ddfe1bae1dab778d8881c2e980f506e
Mawfyy <94380448+Mawfyy@users.noreply.github.com>
2023-12-08 21:01:44 -0500

    fix: fixed context from enable command

--- src/music_events/mod.rs
+++ src/music_events/mod.rs
@@ -21,7 +21,7 @@ pub async fn track_start(_: LavalinkClient, _: String, _: &events::TrackStart) {
 pub async fn track_end(client: LavalinkClient, _: String, track_ended: &TrackEnd) {
     println!("{:?}", &track_ended.guild_id);
     let player = client.get_player_context(track_ended.guild_id).unwrap();
-    let data = client.user_data.read();
+    let data = player.user_data.read();
     let track = data.get::<Track>().unwrap();
 
     println!("{}", track.enable_loop);
