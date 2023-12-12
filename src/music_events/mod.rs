7f0c4a428fb14d2309b38f58b282278ab6fea1f1
Mawfyy <94380448+Mawfyy@users.noreply.github.com>
2023-12-12 13:10:01 -0500

    fix: handling panic in events and update .env.example

--- src/music_events/mod.rs
+++ src/music_events/mod.rs
@@ -22,9 +22,9 @@ pub async fn track_start(_: LavalinkClient, _: String, _: &events::TrackStart) {
 pub async fn track_end(client: LavalinkClient, _: String, track_ended: &TrackEnd) {
     let player = client.get_player_context(track_ended.guild_id).unwrap();
     let data = player.user_data.read();
-    let track = data.get::<Track>().unwrap();
-
-    if track.enable_loop {
-        let _ = player.set_queue(QueueMessage::PushToFront(track_ended.track.clone().into()));
+    if let Some(track) = data.get::<Track>() {
+         if track.enable_loop {
+            let _ = player.set_queue(QueueMessage::PushToFront(track_ended.track.clone().into()));
+        }   
     }
 }
